baa1b932bac1cc199bf1058ab58736724fd66717
tangzihang1 <tangzihang1@jd.com>
2023-08-30 14:22:32 +0800

    preload 路径自适应

--- electron/main/main.js
+++ electron/main/main.js
@@ -1,13 +1,13 @@
 const { app, BrowserWindow, ipcMain, clipboard } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
     // Create the browser window.
     const win = new BrowserWindow({width: 400, height: 732, title: 'copyAchat', webPreferences: {
         contextIsolation: false,
-        preload: '/Users/ganson/personal_dev/copyAchat/electron/main/preload.js',
+        preload: path.join(__dirname, 'preload.js'), // '/Users/tangzihang1/copyAchat/electron/main/preload.js',
         nodeIntegration: true
     }})
-
     // and load the index.html of the app.
     // win.webContents.setUserAgent("Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko; googleweblight) Chrome/38.0.1025.166 Mobile Safari/535.19");
     // win.loadURL('https://www.xiaohongshu.com/')
@@ -19,7 +19,7 @@ const createWindow = () => {
     
     win.loadURL('http://localhost:5173/')
     // 开启开发者工具
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
     const history = []
 
     // 定时任务，每 500 毫秒监听一次剪贴板变化
