21426ad1612fc1032d738d35286e72b27fb91d98
tangzihang1 <tangzihang1@jd.com>
2023-08-30 15:52:08 +0800

    大模型流式响应限时取消,暂时不会出现回答着回答着中断的情况;

--- src/views/RedBook/data.js
+++ src/views/RedBook/data.js
@@ -32,14 +32,14 @@ const chat = async (messageList, apiKey) => {
             {   
                 
                 method: "post",
-                signal: AbortSignal.timeout(8000),
+                // signal: AbortSignal.timeout(8000),
                 // 开启后到达设定时间会中断流式输出
                 headers: {
                     "Content-Type": "application/json",
                     Authorization: `Bearer ${apiKey}`,
                 },
                 body: JSON.stringify({
-                    model: "gpt-3.5-turbo",
+                    model: "gpt-3.5-turbo-16k",
                     stream: true,
                     messages: messageList,
                 }),
